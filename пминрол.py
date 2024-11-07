f1d76b3d5de17e28fcb7d4cbf93b5a0f9430f8b2
aleshka0801 <alexgrigoryev2011@gmail.com>
2024-11-07 10:00:02 +0700

    Add files via upload

---
+++ пминрол.py
@@ -0,0 +1,65 @@
+# def is_evan(num):
+#     res = num %2 == 0
+#     return res
+# print(is_evan(4))
+#
+#
+#
+# def is_evan(num
+#     return  num %2 == 0
+# print(is_evan(8))
+#def valyta(falyta , curse):
+    #try:
+        #return falyta / curse
+        #except ZeroDivisionError:
+        #return'сходи к доктору'
+#falyta = int(input('скоко денег'))
+#curse = int(input('zкурс валюты'))
+#print(valyta(falyta,curse))
+
+#def degree(number,multiplier):
+    #try:
+        #return  number**multiplier
+#number = int(input('число '))
+#multiplier = int(input('степень'))
+#print(degree(number,multiplier))
+#import math
+#print(math.factorial(750))
+# def amount():
+#     weight = int(input('масса вещества'))
+#     molarmass = int(input('vолярная масса'))
+#     if weight > 0:
+#         print('сходи к врачу')
+#     elif molarmass > 0:
+#         print('сходи к врачу')
+#     try:
+#         return weight/molarmass
+#     except ZeroDivisionError:
+#         print('операция невозможна вы ввели 0')
+#         print(weight / molarmass)
+# def  molarmassa():
+#     weight1 = int(input('масса вещества'))
+#     amount2  = int(input('vолярная масса'))
+#     if weight1> 0:
+#         print('сходи к врачу')
+#     elif amount2 > 0:
+#         print('сходи к врачу')
+#     try:
+#         return weight1/
+#     except ZeroDivisionError:
+#         print('операция невозможна вы ввели 0')
+#         print(weight / molarmass)
+# import math
+#
+#
+# def square(radius):
+#     return math.pi * radius ** 2
+#
+#
+# radius = int(input())
+# print(square(radius))
+import math
+C = 12.01115
+O = 15.9994
+mass_CO2 = C + 2 * O
+print(math.ceil(mass_CO2))
